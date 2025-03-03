3ff333cb7dae5bbc45624031340a371f952ab0b5
Romark <107486135+rgc26@users.noreply.github.com>
2025-03-03 11:40:52 +0800

    WebSys-Prelim-Lab

---
+++ websys/main.js
@@ -0,0 +1,302 @@
+// syntax for var
+// var variablename ="variableValue";
+
+
+// var a = 10;
+// var b = 20;
+// console.log(a);
+// console.log(b);
+
+
+
+
+// function example() {
+//     if (true) {
+//         let x = 10;
+//     }
+// console.log(x);
+// }
+// example();
+
+
+
+
+
+
+
+
+// console.log(a);
+// let a = 10;
+// console.log(a);
+
+
+
+
+
+// let name = "John";
+// let name = "Doe";
+// console.log(name);
+
+
+
+
+// for ( let i = 0 ; i < 10 ; i++) {
+//     setTimeout(() => console.log(i), 1000);
+// }
+
+
+
+
+
+// let syntax
+// let variablename ="variableValue";
+
+
+
+
+// {
+//  let num = 10;
+//     console.log(num);
+// } // block scope
+
+// console.log(num); // ReferenceError: num is not defined
+
+
+
+// function fun() {
+//     let a = 10;
+//     console.log(a);
+// }   
+
+// fun();
+// console.log(a); // ReferenceError: a is not defined
+
+
+
+
+
+// let num = 10;
+// console.log(num);
+
+// function fun() {
+//     console.log(num);
+// }
+
+// fun();
+
+
+
+
+
+
+// let x = 77;
+
+// {
+//     let x = 10;
+//     console.log(x);
+// }
+
+// let x = 20;
+
+// console.log(x);
+
+
+
+
+
+
+
+
+
+// x = 12;
+
+// console.log(x);
+// let x;
+
+
+
+
+
+
+
+
+
+
+
+// syntax const
+// const variablename = "variableValue";
+
+
+
+
+
+
+// let x = 10;
+// x = 20;
+// x += 10;
+// console.log(x);
+
+
+
+
+
+// if (true)  {
+//     const x = 10;
+//     console.log(x);
+// }
+
+// console.log(x); // ReferenceError: x is not defined
+
+
+
+
+
+
+
+
+
+
+
+// const x = 10;
+// console.log(x);
+
+
+
+
+
+
+// const person  = {
+//     name: "John",
+//     age: 30
+// };
+
+// Object.preventExtensions(person);
+
+// //attempt to change the value of person
+// person.age = 40;
+// console.log(person.age);
+
+// //attempt to add new property
+// person.city = "New York";
+// console.log(person.city);
+
+// //attempt to delete property
+// delete person.name;
+// console.log(person.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = 10;
+// let num2 = 20.5;
+// let num3 = -30;
+
+// console.log(typeof num1);
+
+
+// //special numeric values
+// console.log(1 / 0); 
+// console.log(-1 / 0);
+// console.log("hello" * 0);
+
+
+
+
+
+
+
+
+
+
+
+// let str1 = "Hello";
+// let str2 = 'World';
+// let str3 = `Hello World`; // backtick
+
+// console.log(typeof str1);
+
+
+// //strinbg concatenation
+// let str = "Hello" +" "+ "World";
+// console.log(str);
+
+
+
+// //string interpolation
+// let age = 30;
+// console.log(`I am ${age} years old.`);
+
+
+
+
+
+// let isJavaScriptFun = true;
+// let isFishTasty = false;
+
+// console.log(typeof isJavaScriptFun);
+
+// //boolean expression
+// let x = 10, y = 20;
+// console.log(x > y); 
+// console.log(x < y);
+// console.log(x == y);
+// console.log(x != y);
+
+
+
+
+
+
+
+
+
+// let emptyValue = null;
+// console.log(emptyValue);
+// console.log(typeof emptyValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let person = {
+    name: "John",
+    age: 30,
+    city: "New York",
+    isStudent: false
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+console.log(person.isStudent);
+console.log(typeof person);
+
+
+
+
+
+
+
+
+
